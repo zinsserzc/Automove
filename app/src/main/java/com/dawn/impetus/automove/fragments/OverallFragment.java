8627abde765ef1f7d89445e38b8fc7f3830d78f8
zinsserzc <576134494@qq.com>
2017-11-10 15:56:30 +0800

    总览界面

--- app/src/main/java/com/dawn/impetus/automove/fragments/OverallFragment.java
+++ app/src/main/java/com/dawn/impetus/automove/fragments/OverallFragment.java
@@ -1,6 +1,7 @@
 package com.dawn.impetus.automove.fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.dawn.impetus.automove.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,11 +22,78 @@ import com.dawn.impetus.automove.R;
 public class OverallFragment extends Fragment {
 
 
+    //图表
+    private PieChart chartCPU;
+    private PieChart charRAM;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = View.inflate(OverallFragment.this.getActivity(),R.layout.fragment_overall,null);
+        initView(view);
+        init();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overall, container, false);
+        return view;
+
+    }
+
+    private void init() {
+        drawChart();
+    }
+
+    private void drawChart() {
+        //pieChart
+        ArrayList<PieEntry> entriesCPU = new ArrayList<>();
+        entriesCPU.add(new PieEntry(18.5f,"Green"));
+        entriesCPU.add(new PieEntry(81.5f,"Red"));
+
+        ArrayList<Integer> colorsCPU = new ArrayList<>();
+        colorsCPU.add(Color.BLUE);
+        colorsCPU.add(Color.CYAN);
+
+        PieDataSet setCPU = new PieDataSet(entriesCPU,"CPU利用率");
+        setCPU.setDrawValues(false);
+        PieData dataCPU = new PieData(setCPU);
+        setCPU.setColors(colorsCPU);
+        chartCPU.setData(dataCPU);
+        chartCPU.setUsePercentValues(false);
+        chartCPU.setDescription("");
+        chartCPU.setBackgroundColor(Color.alpha(0));
+        chartCPU.setDrawEntryLabels(false);
+        chartCPU.setDrawHoleEnabled(true);
+        chartCPU.setDrawCenterText(true);
+        chartCPU.setCenterText("18.5%");
+        chartCPU.setCenterTextColor(Color.BLACK);
+        chartCPU.invalidate();
+
+        //ramChart
+        ArrayList<PieEntry> entriesRAM = new ArrayList<>();
+        entriesRAM.add(new PieEntry(60f,"Green"));
+        entriesRAM.add(new PieEntry(40f,"Red"));
+
+        ArrayList<Integer> colorsRAM = new ArrayList<>();
+        colorsRAM.add(Color.BLUE);
+        colorsRAM.add(Color.CYAN);
+
+        PieDataSet setRAM = new PieDataSet(entriesRAM,"CPU利用率");
+        setRAM.setDrawValues(false);
+        PieData dataRAM = new PieData(setRAM);
+        setRAM.setColors(colorsRAM);
+        charRAM.setData(dataRAM);
+        charRAM.setUsePercentValues(false);
+        charRAM.setDescription("");
+        charRAM.setBackgroundColor(Color.alpha(0));
+        charRAM.setDrawEntryLabels(false);
+        charRAM.setDrawHoleEnabled(true);
+        charRAM.setDrawCenterText(true);
+        charRAM.setCenterText("60%");
+        charRAM.setCenterTextColor(Color.BLACK);
+        charRAM.invalidate();
+    }
+
+    private void initView(View view) {
+        chartCPU =  (PieChart) view.findViewById(R.id.piechart_cpu);
+        charRAM = (PieChart) view.findViewById(R.id.piechart_ram);
     }
 
 }
