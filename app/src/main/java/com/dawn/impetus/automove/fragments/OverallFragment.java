5e3726f88bf92cf9895045589f4c485da9883ebb
zinsserzc <576134494@qq.com>
2017-11-24 17:00:40 +0800

    完成语音识别封装

--- app/src/main/java/com/dawn/impetus/automove/fragments/OverallFragment.java
+++ app/src/main/java/com/dawn/impetus/automove/fragments/OverallFragment.java
@@ -19,6 +19,7 @@ import com.baidu.speech.EventManager;
 import com.baidu.speech.EventManagerFactory;
 import com.baidu.speech.asr.SpeechConstant;
 import com.dawn.impetus.automove.R;
+import com.dawn.impetus.automove.utils.VoiceUtil;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -33,17 +34,16 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * A simple {@link Fragment} subclass.
+ * 总览
  */
-public class OverallFragment extends Fragment implements View.OnClickListener,EventListener{
+public class OverallFragment extends Fragment implements View.OnClickListener{
 
     private ImageView iconSearch;
 
 
     //Baidu语音
-    private EventManager asr;
-    private boolean isStart = false;
-    private String msgContent = "";
+    private VoiceUtil voice;
+    private boolean isStart;
 
     //图表
     private PieChart chartCPU;
@@ -63,10 +63,8 @@ public class OverallFragment extends Fragment implements View.OnClickListener,Ev
     private void init() {
         iconSearch.setOnClickListener(this);
 
-        //初始化百度语音manager
-        initPermission();
-        asr = EventManagerFactory.create(this.getActivity(),"asr");
-        asr.registerListener(this);
+        //初始百度语音
+        voice = new VoiceUtil(this.getActivity());
 
         drawChart();
 
@@ -135,9 +133,9 @@ public class OverallFragment extends Fragment implements View.OnClickListener,Ev
             case R.id.icon_search:
                 isStart = !isStart;
                 if(isStart){
-                    start();
+                    voice.start();
                 }else {
-                    stop();
+                    voice.stop();
                 }
                 break;
             default:
@@ -146,103 +144,11 @@ public class OverallFragment extends Fragment implements View.OnClickListener,Ev
 
     }
 
-    private void start(){
-        Map<String,Object> params = new LinkedHashMap<>();
-        String event = null;
-        event = SpeechConstant.ASR_START;
-
-        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME,false);
-        params.put(SpeechConstant.VAD,SpeechConstant.VAD_TOUCH);
-        String json = null; //可以替换成自己的json
-        json = new JSONObject(params).toString();
-        Toast.makeText(this.getActivity(),"开始说话",Toast.LENGTH_SHORT).show();
-        asr.send(event, json, null, 0, 0);
-    }
-
-    private void stop(){
-        Toast.makeText(this.getActivity(),"结束说话",Toast.LENGTH_SHORT).show();
-        asr.send(SpeechConstant.ASR_STOP, null, null, 0, 0); //
-    }
-
-    @Override
-    public void onEvent(String name, String params, byte[] data, int offset, int length) {
-        String logTxt = "name: " + name;
-        //自动识别
-//        if (params != null && !params.isEmpty()) {
-//            logTxt += " ;params :" + params;
-//        }
-//        if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_PARTIAL)) {
-//            if (params.contains("\"nlu_result\"")) {
-//                if (length > 0 && data.length > 0) {
-//                    logTxt += ", 语义解析结果：" + new String(data, offset, length);
-//                }
-//            }
-//        } else if (data != null) {
-//            logTxt += " ;data length=" + data.length;
-//        }
-        if(name.equals(SpeechConstant.CALLBACK_EVENT_ASR_READY)){
-            // 引擎就绪，可以说话，一般在收到此事件后通过UI通知用户可以说话了
-            showMessage("请开始你的表演");
-        }
-
-        if(name.equals(SpeechConstant.CALLBACK_EVENT_ASR_FINISH)){
-            // 识别结束
-            showMessage("请等待结果");
-            try{
-                JSONObject resultJson = new JSONObject(msgContent);
-                showMessage(resultJson.getString("best_result"));
-            }catch (JSONException e){
-                showMessage("识别失败！");
-            }
-        }
-
-        if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_PARTIAL)) {
-            if(params != null && !params.isEmpty()){
-                msgContent = params;
-            }
-        }
-        //showMessage(logTxt);
-    }
-
-    private void showMessage(String text){
-        Toast.makeText(this.getActivity(),text,Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * android 6.0 以上需要动态申请权限
-     */
-    private void initPermission() {
-        String permissions[] = {Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.ACCESS_NETWORK_STATE,
-                Manifest.permission.INTERNET,
-                Manifest.permission.READ_PHONE_STATE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
-
-        ArrayList<String> toApplyList = new ArrayList<String>();
-
-        for (String perm :permissions){
-            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this.getActivity(), perm)) {
-                toApplyList.add(perm);
-                //进入到这里代表没有权限.
-
-            }
-        }
-        String tmpList[] = new String[toApplyList.size()];
-        if (!toApplyList.isEmpty()){
-            ActivityCompat.requestPermissions(this.getActivity(), toApplyList.toArray(tmpList), 123);
-        }
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-    }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        asr.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
+        voice.destroy();
 
     }
 }
