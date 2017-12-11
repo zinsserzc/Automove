e2302203530daa1d5aa782b7e8c5b002604dadb1
linsg <linsg999@126.com>
2017-12-11 11:20:15 +0800

    解决ui重复创建问题,添加作业长按删除功能(未测试)

--- app/src/main/java/com/dawn/impetus/automove/fragments/WorkFragment.java
+++ app/src/main/java/com/dawn/impetus/automove/fragments/WorkFragment.java
@@ -4,7 +4,10 @@ package com.dawn.impetus.automove.fragments;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,46 +32,71 @@ import java.util.List;
  */
 public class WorkFragment extends Fragment {
 
+    private View rootView;
+
     private ListView jobListView;
     private JobAdapter jobAdapter;
-    List<Job> jobs;
+    static List<Job> jobs;
+    private Handler jobsRefreshHandler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = View.inflate(WorkFragment.this.getActivity(), R.layout.fragment_work, null);
-        jobListView = (ListView) view.findViewById(R.id.jobListView);
+        
 
-        init();
-        setListItemDeleMethod();
+        if (rootView == null) {
+            rootView = View.inflate(WorkFragment.this.getActivity(), R.layout.fragment_work, null);
+            jobListView = (ListView) rootView.findViewById(R.id.jobListView);
 
-        return view;
-        //return inflater.inflate(R.layout.fragment_work, container, false);
+            init();
+            setListItemDeleMethod();
+            //设置list更新处理handler
+            jobsRefreshHandler = new Handler() {
+
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    Toast.makeText(WorkFragment.this.getActivity(), "作业已更新", Toast.LENGTH_SHORT).show();
+                    if (jobs.size() == 0) {
+                        Toast.makeText(WorkFragment.this.getActivity(), "暂无作业", Toast.LENGTH_SHORT).show();
+                    }
+                    jobAdapter.notifyDataSetChanged();
+                }
+
+            };
+
+        }
+        //用来避免fragment重复创建问题
+        ViewGroup parent = (ViewGroup) rootView.getParent();
+        if (parent != null) {
+            parent.removeView(rootView);
+        }
+
+        return rootView;
     }
 
     private void setListItemDeleMethod() {
-        jobListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+        jobListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, final View view,
                                            final int position, long id) {
                 // TODO Auto-generated method stub
                 // When clicked, show a toast with the TextView text
-                AlertDialog.Builder builder=new AlertDialog.Builder(WorkFragment.this.getActivity());
+                AlertDialog.Builder builder = new AlertDialog.Builder(WorkFragment.this.getActivity());
                 builder.setMessage("确认删除?");
 
-                builder.setPositiveButton("是",new DialogInterface.OnClickListener(){
+                builder.setPositiveButton("是", new DialogInterface.OnClickListener() {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
-                        String jobID=((TextView)view.findViewById(R.id.jobName)).getText().toString();
+                        String jobID = ((TextView) view.findViewById(R.id.jobName)).getText().toString();
 
 //                        if(ServerUtil.deleteJob(jobID))
 //                        {
-                            jobs.remove(position);
-                            jobAdapter.notifyDataSetChanged();
-                            Toast.makeText(WorkFragment.this.getActivity(),"删除job成功",Toast.LENGTH_SHORT).show();
+                        jobs.remove(position);
+                        jobAdapter.notifyDataSetChanged();
+                        Toast.makeText(WorkFragment.this.getActivity(), "删除job成功", Toast.LENGTH_SHORT).show();
 //                        }else {
 //
 //                            Toast.makeText(WorkFragment.this.getActivity(),"删除job失败",Toast.LENGTH_SHORT).show();
@@ -77,7 +105,7 @@ public class WorkFragment extends Fragment {
                     }
 
                 });
-                builder.setNegativeButton("取消",new DialogInterface.OnClickListener(){
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -88,41 +116,65 @@ public class WorkFragment extends Fragment {
                 });
                 builder.create().show();
                 return false;
-            }});
+            }
+        });
 
     }
 
     private void init() {
 
-        jobs=new ArrayList<>();
-        jobAdapter =new JobAdapter();
+        //初始化listview
+        jobs = new ArrayList<>();
+        jobAdapter = new JobAdapter();
         jobListView.setAdapter(jobAdapter);
 
-        if(jobs.size()==0)
-        {
-            Toast.makeText(WorkFragment.this.getActivity(),"任务获取中",Toast.LENGTH_LONG).show();
+        if (jobs.size() == 0) {
+            Toast.makeText(WorkFragment.this.getActivity(), "作业获取中", Toast.LENGTH_LONG).show();
         }
 
-        Runnable updateUITask = new Runnable() {
+
+        Runnable updateList = new Runnable() {
             @Override
             public void run() {
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        jobs = ServerUtil.getJobsInfo();
-                        if (jobs.size()==0)
-                        {
-                            Toast.makeText(WorkFragment.this.getActivity(),"暂无任务",Toast.LENGTH_SHORT).show();
-                        }
-                        jobAdapter.notifyDataSetChanged();
-//                        jobAdapter = new JobAdapter();
-//                        jobListView.setAdapter(jobAdapter);
+
+                while (true) {
+
+                    jobs = ServerUtil.getJobsInfo();
+                    Message msg = new Message();
+                    jobsRefreshHandler.sendMessage(msg);
+                    try {
+                        Thread.sleep(1000 * 60);
+                    } catch (Exception e) {
+
                     }
-                });
+
+                }
+
             }
         };
+        ThreadManager.THREAD_POOL_EXECUTOR.execute(updateList);
 
-        ThreadManager.THREAD_POOL_EXECUTOR.execute(updateUITask);
+
+//        Runnable updateUITask = new Runnable() {
+//            @Override
+//            public void run() {
+//                getActivity().runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        jobs = ServerUtil.getJobsInfo();
+//                        if (jobs.size()==0)
+//                        {
+//                            Toast.makeText(WorkFragment.this.getActivity(),"暂无任务",Toast.LENGTH_SHORT).show();
+//                        }
+//                        jobAdapter.notifyDataSetChanged();
+////                        jobAdapter = new JobAdapter();
+////                        jobListView.setAdapter(jobAdapter);
+//                    }
+//                });
+//            }
+//        };
+//
+//        ThreadManager.THREAD_POOL_EXECUTOR.execute(updateUITask);
 
     }
 
